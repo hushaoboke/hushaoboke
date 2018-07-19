f63e4b7e2b2b03be906662aaaf463f4103a01dbd
“hujinyuan66” <1776588731@qq.com>
2018-07-19 10:21:46 +0800

    Site updated: 2018-07-19 10:21:44

---
+++ dist/music.js
@@ -0,0 +1,63 @@
+const ap = new APlayer({
+    container: document.getElementById('aplayer'),
+    fixed: true,
+    autoplay: false,
+    audio: [
+      
+	     {
+        name: 'Something Just Like This',
+        artist: 'Hs',
+        url: 'http://music.163.com/song/media/outer/url?id=461347998.mp3',
+        cover: 'http://p1.music.126.net/ggnyubDdMxrhpqYvpZbhEQ==/3302932937412681.jpg',
+      },
+	   {
+        name: 'keneng',
+        artist: 'Hs',
+        url: 'http://music.163.com/song/media/outer/url?id=569214126.mp3',
+        cover: 'http://p1.music.126.net/SJYnDay7wgewU3O7tPfmOQ==/109951163322541581.jpg',
+      },
+	  
+	  
+	    {
+        name: 'baiyang',
+        artist: 'Hs',
+        url: 'http://music.163.com/song/media/outer/url?id=514761281.mp3',
+        cover: 'http://p1.music.126.net/tczb_7II9KzSuLQsVt89Gw==/109951163049336667.jpg',
+      },
+	 
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+	  {
+        name: "langren",
+        artist: 'Hs',
+        url: 'http://music.163.com/song/media/outer/url?id=566442496.mp3',
+        cover: 'http://p1.music.126.net/G5YxAyt9812z9MROfWarkg==/109951163318974870.jpg',
+      },
+      
+	  {
+        name: 'wanghou',
+        artist: 'Hs',
+        url: 'http://music.163.com/song/media/outer/url?id=571338279.mp3',
+        cover: 'http://p1.music.126.net/hTiVEeQTUSsc-YGF3o6ItQ==/109951163337847600.jpg',
+      },
+      {
+        name: 'bali',
+        artist: 'Hs',
+        url: 'http://music.163.com/song/media/outer/url?id=864647841.mp3',
+        cover: 'http://p1.music.126.net/zHc8QgE0TLI1nQnWeft0Yw==/109951163403299614.jpg',
+      }
+    ]
+});
