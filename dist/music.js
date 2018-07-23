6ba18d011c7aed16d82e9f3af89dc1dc67bcbbaa
“hujinyuan66” <1776588731@qq.com>
2018-07-23 09:46:02 +0800

    Site updated: 2018-07-23 09:46:01

--- dist/music.js
+++ dist/music.js
@@ -5,28 +5,37 @@ const ap = new APlayer({
     audio: [
       
 	     {
-        name: 'Something Just Like This',
-        artist: 'Hs',
+        name: ' 追光者 ',
+        artist: ' Cover 岑宁儿 ',
+        url: 'http://music.163.com/song/media/outer/url?id=505771816.mp3',
+        cover: 'http://p1.music.126.net/6x6bCsbTsBKydOtdUoIMAQ==/109951163024132338.jpg',
+      },
+	  
+	  
+
+
+
+		{
+        name: ' Something Just Like This ',
+        artist: ' The Chainsmokers,Coldplay ',
         url: 'http://music.163.com/song/media/outer/url?id=461347998.mp3',
         cover: 'http://p1.music.126.net/ggnyubDdMxrhpqYvpZbhEQ==/3302932937412681.jpg',
       },
 	   {
-        name: 'keneng',
-        artist: 'Hs',
+        name: ' 可能否 ',
+        artist: ' 木小雅 ',
         url: 'http://music.163.com/song/media/outer/url?id=569214126.mp3',
         cover: 'http://p1.music.126.net/SJYnDay7wgewU3O7tPfmOQ==/109951163322541581.jpg',
       },
 	  
 	  
 	    {
-        name: 'baiyang',
-        artist: 'Hs',
+        name: ' 白羊 ',
+        artist: ' 徐秉龙,沈以诚 ',
         url: 'http://music.163.com/song/media/outer/url?id=514761281.mp3',
         cover: 'http://p1.music.126.net/tczb_7II9KzSuLQsVt89Gw==/109951163049336667.jpg',
       },
-	 
-	  
-	  
+	
 	  
 	  
 	  
@@ -41,21 +50,21 @@ const ap = new APlayer({
 	  
 	  
 	  {
-        name: "langren",
-        artist: 'Hs',
+        name: " 浪人琵琶 ",
+        artist: ' 胡66 ',
         url: 'http://music.163.com/song/media/outer/url?id=566442496.mp3',
         cover: 'http://p1.music.126.net/G5YxAyt9812z9MROfWarkg==/109951163318974870.jpg',
       },
       
 	  {
-        name: 'wanghou',
-        artist: 'Hs',
+        name: ' 往后余生（翻自 马良）',
+        artist: ' 王贰浪 ',
         url: 'http://music.163.com/song/media/outer/url?id=571338279.mp3',
         cover: 'http://p1.music.126.net/hTiVEeQTUSsc-YGF3o6ItQ==/109951163337847600.jpg',
       },
       {
-        name: 'bali',
-        artist: 'Hs',
+        name: ' 一个人去巴黎 ',
+        artist: ' 董又霖 ',
         url: 'http://music.163.com/song/media/outer/url?id=864647841.mp3',
         cover: 'http://p1.music.126.net/zHc8QgE0TLI1nQnWeft0Yw==/109951163403299614.jpg',
       }
