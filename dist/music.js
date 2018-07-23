de311ebd1641b2480678da285d0515c28b397483
“hujinyuan66” <1776588731@qq.com>
2018-07-23 16:38:03 +0800

    Site updated: 2018-07-23 16:38:03

--- dist/music.js
+++ dist/music.js
@@ -4,13 +4,103 @@ const ap = new APlayer({
     autoplay: false,
     audio: [
       
-	     {
+	     
+
+
+
+
+		 {
+        name: ' That Girl ',
+        artist: ' Olly Murs ',
+        url: 'http://music.163.com/song/media/outer/url?id=440208476.mp3',
+        cover: 'http://p1.music.126.net/_dPhSlbDz23MjXUEYeBGRw==/18820340533734696.jpg',
+      },
+	  
+ {
+        name: ' Dream It Possible ',
+        artist: ' Delacey ',
+        url: 'http://music.163.com/song/media/outer/url?id=38592976.mp3',
+        cover: 'http://p1.music.126.net/Gfq_eVuq7lgilBf0w1g5dg==/109951163088676710.jpg',
+      },
+	   {
+        name: ' BINGBIAN病变 ',
+        artist: ' Cubi / Fi9江澈 / Younglife ',
+        url: 'http://music.163.com/song/media/outer/url?id=533259686.mp3',
+        cover: 'http://p1.music.126.net/OVBzkUhphxfWfkji9Owhkw==/109951163118002763.jpg',
+      },
+	  
+	  
+
+		{
+        name: ' 喜欢 ',
+        artist: ' 阿肆 ',
+        url: 'http://music.163.com/song/media/outer/url?id=526464145.mp3',
+        cover: 'http://p1.music.126.net/BA0bFrD0cK4YDSYublCThA==/109951163093942268.jpg',
+      },
+	  {
+        name: ' Lemon ',
+        artist: ' 米津玄師 ',
+        url: 'http://music.163.com/song/media/outer/url?id=536622304.mp3',
+        cover: 'http://p1.music.126.net/r0TgUXBEEmMG48KSsEa_mg==/109951163143657970.jpg',
+      },
+	  
+	  {
+        name: ' 学猫叫 ',
+        artist: ' 小潘潘 / 小峰峰 ',
+        url: 'http://music.163.com/song/media/outer/url?id=554191055.mp3',
+        cover: 'http://p1.music.126.net/D1Ov-XMAwUzsr16mQk95fA==/109951163256119128.jpg',
+      },
+	   {
+        name: ' 成都 ',
+        artist: ' 赵雷 ',
+        url: 'http://music.163.com/song/media/outer/url?id=436514312.mp3',
+        cover: 'http://p1.music.126.net/34YW1QtKxJ_3YnX9ZzKhzw==/2946691234868155.jpg',
+      },
+	   {
+        name: ' 我们不一样 ',
+        artist: ' 大壮 ',
+        url: 'http://music.163.com/song/media/outer/url?id=482395261.mp3',
+        cover: 'http://p1.music.126.net/e8rrwkOED4RbeaKuaVOcJA==/18997361904874206.jpg',
+      },
+	  {
+        name: ' 差一步 ',
+        artist: ' 大壮 ',
+        url: 'http://music.163.com/song/media/outer/url?id=518076124.mp3',
+        cover: 'http://p1.music.126.net/b4MrcWDaX7MmdLlfoT-UcQ==/109951163061728442.jpg',
+      },
+	  
+	  
+	  {
+        name: ' 安和桥 ',
+        artist: ' 宋冬野 ',
+        url: 'http://music.163.com/song/media/outer/url?id=27646205.mp3',
+        cover: 'http://p1.music.126.net/GcRunGm02vZBicYmIN6GXw==/109951163200249252.jpg',
+      },
+	   {
+        name: ' 理想三旬 ',
+        artist: ' 陈鸿宇 ',
+        url: 'http://music.163.com/song/media/outer/url?id=31445772.mp3',
+        cover: 'http://p1.music.126.net/cqTTEPAaxXG3cOwaE4E_-g==/109951163104103366.jpg',
+      },
+	   {
+        name: ' 不找了 ',
+        artist: ' 郭旭 ',
+        url: 'http://music.163.com/song/media/outer/url?id=29850531.mp3',
+        cover: 'http://p1.music.126.net/o25ptaSygDb2rVdTpIKexw==/17793396672429120.jpg',
+      },
+	  {
+        name: ' 平凡之路 ',
+        artist: ' 朴树 ',
+        url: 'http://music.163.com/song/media/outer/url?id=28815250.mp3',
+        cover: 'http://p1.music.126.net/W_5XiCv3rGS1-J7EXpHSCQ==/18885211718782327.jpg',
+      },
+	  
+	  {
         name: ' 追光者 ',
         artist: ' Cover 岑宁儿 ',
         url: 'http://music.163.com/song/media/outer/url?id=505771816.mp3',
         cover: 'http://p1.music.126.net/6x6bCsbTsBKydOtdUoIMAQ==/109951163024132338.jpg',
       },
-	  
 	  
 
 
