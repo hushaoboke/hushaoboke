e0bae16bb52f7cfca6a90b82e873a47b25433f8c
“hujinyuan66” <1776588731@qq.com>
2018-12-22 23:47:32 +0800

    Site updated: 2018-12-22 23:47:30

---
+++ dist/music.js
@@ -0,0 +1,176 @@
+const ap = new APlayer({
+    container: document.getElementById('aplayer'),
+    fixed: true,
+    autoplay: false,
+    audio: [     
+
+	{
+        name: ' Lemon ',
+        artist: ' 米津玄師 ',
+        url: 'http://music.163.com/song/media/outer/url?id=536622304.mp3',
+        cover: 'http://p1.music.126.net/r0TgUXBEEmMG48KSsEa_mg==/109951163143657970.jpg',
+        },
+     
+
+
+	 {
+        name: ' 在人间 ',
+        artist: ' 王建房 ',
+        url: 'http://h5.1ting.com/file?url=/zzzzzmp3/2014aJan/13W/13wangjianfang/03.mp3',
+        cover: 'http://img.1ting.com/images/special/345/s300_1466f111bb2a4b864118143103f6e723.jpg',
+        },
+	 
+	
+	
+	  
+   
+
+
+
+	  {
+        name: ' That Girl ',
+        artist: ' Olly Murs ',
+        url: 'http://music.163.com/song/media/outer/url?id=440208476.mp3',
+        cover: 'http://p1.music.126.net/_dPhSlbDz23MjXUEYeBGRw==/18820340533734696.jpg',
+      },
+	  
+       {         
+        name: ' Dream It Possible ',
+        artist: ' Delacey ',
+        url: 'http://music.163.com/song/media/outer/url?id=38592976.mp3',
+        cover: 'http://p1.music.126.net/Gfq_eVuq7lgilBf0w1g5dg==/109951163088676710.jpg',
+      },
+	   {
+        name: ' BINGBIAN病变 ',
+        artist: ' Cubi / Fi9江澈 / Younglife ',
+        url: 'http://music.163.com/song/media/outer/url?id=533259686.mp3',
+        cover: 'http://p1.music.126.net/OVBzkUhphxfWfkji9Owhkw==/109951163118002763.jpg',
+      },
+	
+
+
+
+		{
+        name: ' 喜欢 ',
+        artist: ' 阿肆 ',
+        url: 'http://music.163.com/song/media/outer/url?id=526464145.mp3',
+        cover: 'http://p1.music.126.net/BA0bFrD0cK4YDSYublCThA==/109951163093942268.jpg',
+      },
+	  
+	  {
+        name: ' 学猫叫 ',
+        artist: ' 小潘潘 / 小峰峰 ',
+        url: 'http://music.163.com/song/media/outer/url?id=554191055.mp3',
+        cover: 'http://p1.music.126.net/D1Ov-XMAwUzsr16mQk95fA==/109951163256119128.jpg',
+      },
+	   {
+        name: ' 成都 ',
+        artist: ' 赵雷 ',
+        url: 'http://music.163.com/song/media/outer/url?id=436514312.mp3',
+        cover: 'http://p1.music.126.net/34YW1QtKxJ_3YnX9ZzKhzw==/2946691234868155.jpg',
+      },
+	   {
+        name: ' 我们不一样 ',
+        artist: ' 大壮 ',
+        url: 'http://music.163.com/song/media/outer/url?id=482395261.mp3',
+        cover: 'http://p1.music.126.net/e8rrwkOED4RbeaKuaVOcJA==/18997361904874206.jpg',
+      },
+	  {
+        name: ' 差一步 ',
+        artist: ' 大壮 ',
+        url: 'http://music.163.com/song/media/outer/url?id=518076124.mp3',
+        cover: 'http://p1.music.126.net/b4MrcWDaX7MmdLlfoT-UcQ==/109951163061728442.jpg',
+      },
+	  
+	  
+	  {
+        name: ' 安和桥 ',
+        artist: ' 宋冬野 ',
+        url: 'http://music.163.com/song/media/outer/url?id=27646205.mp3',
+        cover: 'http://p1.music.126.net/GcRunGm02vZBicYmIN6GXw==/109951163200249252.jpg',
+      },
+	   {
+        name: ' 理想三旬 ',
+        artist: ' 陈鸿宇 ',
+        url: 'http://music.163.com/song/media/outer/url?id=31445772.mp3',
+        cover: 'http://p1.music.126.net/cqTTEPAaxXG3cOwaE4E_-g==/109951163104103366.jpg',
+      },
+	   {
+        name: ' 不找了 ',
+        artist: ' 郭旭 ',
+        url: 'http://music.163.com/song/media/outer/url?id=29850531.mp3',
+        cover: 'http://p1.music.126.net/o25ptaSygDb2rVdTpIKexw==/17793396672429120.jpg',
+      },
+	  {
+        name: ' 平凡之路 ',
+        artist: ' 朴树 ',
+        url: 'http://music.163.com/song/media/outer/url?id=28815250.mp3',
+        cover: 'http://p1.music.126.net/W_5XiCv3rGS1-J7EXpHSCQ==/18885211718782327.jpg',
+      },
+	  
+	  {
+        name: ' 追光者 ',
+        artist: ' Cover 岑宁儿 ',
+        url: 'http://music.163.com/song/media/outer/url?id=505771816.mp3',
+        cover: 'http://p1.music.126.net/6x6bCsbTsBKydOtdUoIMAQ==/109951163024132338.jpg',
+      },
+	  
+
+
+
+		{
+        name: ' Something Just Like This ',
+        artist: ' The Chainsmokers,Coldplay ',
+        url: 'http://music.163.com/song/media/outer/url?id=461347998.mp3',
+        cover: 'http://p1.music.126.net/ggnyubDdMxrhpqYvpZbhEQ==/3302932937412681.jpg',
+      },
+	   {
+        name: ' 可能否 ',
+        artist: ' 木小雅 ',
+        url: 'http://music.163.com/song/media/outer/url?id=569214126.mp3',
+        cover: 'http://p1.music.126.net/SJYnDay7wgewU3O7tPfmOQ==/109951163322541581.jpg',
+      },
+	  
+	  
+	    {
+        name: ' 白羊 ',
+        artist: ' 徐秉龙,沈以诚 ',
+        url: 'http://music.163.com/song/media/outer/url?id=514761281.mp3',
+        cover: 'http://p1.music.126.net/tczb_7II9KzSuLQsVt89Gw==/109951163049336667.jpg',
+      },
+	
+
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+	  {
+        name: " 浪人琵琶 ",
+        artist: ' 胡66 ',
+        url: 'http://music.163.com/song/media/outer/url?id=566442496.mp3',
+        cover: 'http://p1.music.126.net/G5YxAyt9812z9MROfWarkg==/109951163318974870.jpg',
+      },
+      
+	  {
+        name: ' 往后余生（翻自 马良）',
+        artist: ' 王贰浪 ',
+        url: 'http://music.163.com/song/media/outer/url?id=571338279.mp3',
+        cover: 'http://p1.music.126.net/hTiVEeQTUSsc-YGF3o6ItQ==/109951163337847600.jpg',
+      },
+      {
+        name: ' 一个人去巴黎 ',
+        artist: ' 董又霖 ',
+        url: 'http://music.163.com/song/media/outer/url?id=864647841.mp3',
+        cover: 'http://p1.music.126.net/zHc8QgE0TLI1nQnWeft0Yw==/109951163403299614.jpg',
+      }
+    ]
+});
